58dbda9162c1dd04d3e72e01b363702d3fc1bcb4
Kamal Hamdani <meripop1@gmail.com>
2025-06-19 21:54:36 -0500

    End of Section 2i

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,17 +1,14 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 
 // Added comment to test git push from cmd
 // Added comment to test git push from VS Code Terminal
 namespace API.Controllers
 {
-    [Route("api/[controller]")] // /api/users
-    [ApiController]
-    public class UsersController(DataContext context) : ControllerBase
+     public class UsersController(DataContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
@@ -24,7 +21,7 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             var user = await context.Users.FindAsync(id);
-            if (user == null) return NotFound();
+           if (user == null) return NotFound();
             return user;
 
         }
