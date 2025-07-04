a7e9419240db3ac7546a89add89d08c08e440930
Kamal Hamdani <meripop1@gmail.com>
2025-07-04 13:54:53 -0500

    End of section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,29 +1,31 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-// Added comment to test git push from cmd
-// Added comment to test git push from VS Code Terminal
-namespace API.Controllers
+
+namespace API.Controllers;
+
+public class UsersController(DataContext context) : BaseApiController
 {
-     public class UsersController(DataContext context) : BaseApiController
+    [AllowAnonymous]
+    // [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-        {
-            var users = await context.Users.ToListAsync();
-            return users;
+        var users = await context.Users.ToListAsync();
+        return users;
 
-        }
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            var user = await context.Users.FindAsync(id);
-           if (user == null) return NotFound();
-            return user;
+    }
+    [Authorize]
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return user;
 
-        }
     }
 }
+
