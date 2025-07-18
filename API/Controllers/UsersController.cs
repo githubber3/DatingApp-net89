b882cd3a6d7515a1d8103a2c23fb1d5389181484
Kamal Hamdani <meripop1@gmail.com>
2025-07-18 11:46:06 -0500

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -9,14 +10,15 @@ using Microsoft.EntityFrameworkCore;
 
 
 namespace API.Controllers;
+
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
         var users = await userRepository.GetMembersAsync();
-        
+
         return Ok(users);
 
     }
@@ -24,10 +26,27 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         var user = await userRepository.GetMemberAsync(username);
-        
+
         if (user == null) return NotFound();
         return user;
 
+    }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+        
+        return BadRequest("Failed to update the user");
     }
 }
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,15 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
+import { NgxSpinnerComponent } from 'ngx-spinner';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent]
+
 })
 export class AppComponent implements OnInit {
-
   private accountService = inject(AccountService);
 
   ngOnInit(): void {
@@ -23,8 +24,5 @@ export class AppComponent implements OnInit {
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-
   }
-
-
 }
